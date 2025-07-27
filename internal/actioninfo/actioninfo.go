a678b6fb13471204abc2a1232151679e26eb13be
Ilya.Karabach <il.karabatch2011@gmail.com>
2025-07-27 20:24:56 +0300

    first commit

--- internal/actioninfo/actioninfo.go
+++ internal/actioninfo/actioninfo.go
@@ -1,9 +1,30 @@
 package actioninfo
 
+import (
+	"fmt"
+	"log"
+)
+
 type DataParser interface {
 	// TODO: добавить методы
+	Parse(datastring string) (err error)
+	ActionInfo() (string, error)
 }
 
 func Info(dataset []string, dp DataParser) {
 	// TODO: реализовать функцию
+	for _, data := range dataset {
+		err := dp.Parse(data)
+		if err != nil {
+			log.Println("Ошибка при парсинге данных:", err)
+			continue
+		}
+		info, err := dp.ActionInfo()
+		if err != nil {
+			log.Println("Ошибка при получении информации:", err)
+			continue
+		}
+		fmt.Println(info)
+	}
+
 }
